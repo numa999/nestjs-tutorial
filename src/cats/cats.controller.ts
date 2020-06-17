ff12f08b30971fc5a974887b6eb5f742c11649f0
ex_onuma <ex_onuma@ex-onuma.local>
2020-06-17 14:27:51 +0900

    provider編集

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,11 +1,21 @@
-import { Controller, Get, HttpCode, Req } from '@nestjs/common';
+import { Controller, Get, HttpCode, Req, Post, Body } from '@nestjs/common';
 import { Request } from 'express';
+import { CatsService } from './cats.service'
+import { Cat } from './interfaces/cat.interface'
+import { CreateCatDto } from './dto/create-cat.dto'
 
 @Controller('cats')
 export class CatsController {
+  constructor(private catsService: CatsService) {}
+
   @Get()
-  findAll(): string {
-    return 'This action returns all cats';
+  async findAll(): Promise<Cat[]> {
+    return this.catsService.findAll()
+  }
+
+  @Post()
+  async create(@Body() createCatDto: CreateCatDto) {
+    this.catsService.create(createCatDto)
   }
 
   @Get('not-implemented')
